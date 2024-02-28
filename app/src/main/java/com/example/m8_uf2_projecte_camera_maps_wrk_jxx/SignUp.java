9f1f0c44512c5b938eb2df29703eff687cbea5cf
jiins8 <117478765+jiins8@users.noreply.github.com>
2024-02-28 09:17:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/m8_uf2_projecte_camera_maps_wrk_jxx/SignUp.java
@@ -0,0 +1,79 @@
+package com.example.m8_uf2_projecte_camera_maps_wrk_jxx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.m8_uf2_projecte_camera_maps_wrk_jxx.databinding.ActivitySignUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUp extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+    private EditText editTextEmail, editTextPassword;
+    private Button sign_up_btn;
+    private FirebaseAuth mAuth;
+    String email, password;
+
+    private TextView click_to_login;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = binding.tvEmail;
+        editTextPassword = binding.tvPassword;
+        sign_up_btn = binding.signUpBtn;
+        click_to_login = binding.loginNow;
+
+        click_to_login.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        });
+
+        sign_up_btn.setOnClickListener(view -> {
+
+            email = String.valueOf(editTextEmail.getText());
+            password = String.valueOf(editTextPassword.getText());
+
+            if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+                Toast.makeText(SignUp.this, "The email and password cannot be empty", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(SignUp.this, "The email cannot be empty", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(SignUp.this, "The password cannot be empty", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        });
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "M8_UF2_PROJECTE_CAMERA_MAPS_WRK_JXX"
+include(":app")
+ 
